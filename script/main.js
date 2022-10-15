c63bc37c035c569e2895874066ad1028461f5458
karolinawasalska <karolina1983_4@hotmail.com>
2022-10-15 10:12:16 +0200

    Constructor Card+Player

--- script/main.js
+++ script/main.js
@@ -1,23 +1,36 @@
-// Modalen Dialog öffnen um Namen einzugeben
+//Modaler Dialog
 let myModal = new bootstrap.Modal(document.getElementById('playerNames'));
 myModal.show();
 
+
+// Constructor Card
+function Card(color, text, value, score) {
+    this.Color = color;
+    this.Text = text;
+    this.Value = value;
+    this.Score = score;
+}
+
+
+// Constructor Player
+function Player(name, cards = [], score = -1) {
+    this.Name = name;
+    this.Cards = cards;
+    this.Score = score;
+}
+
+
+
 let players_global = ["a","b"];
 
-
-// nach jeder tasteneingabe im formular überprüfen ob
-// 4 eindeutige spielerInnennamen vorhanden sind
 document.getElementById('playerNamesForm').addEventListener('keyup', function(evt) {
     console.log(evt);
-
-    //TODO
-
 })
 
-// formular submit abfangen
+
 document.getElementById('playerNamesForm').addEventListener('submit', function(evt){   
  console.log("Spieler hat Button 'Spiel starte' gedrückt!");
-    // Formular absenden verhindern
+    
     evt.preventDefault();
     myModal.hide();
 
@@ -26,14 +39,13 @@ document.getElementById('playerNamesForm').addEventListener('submit', function(e
     console.log("Name von Spieler 3: ", document.getElementById("playerName3").value);
     console.log("Name von Spieler 4: ", document.getElementById("playerName4").value);
     
-    players_global = [document.getElementById("playerName1").value, document.getElementById("playerName2").value, document.getElementById("playerName3").value,document.getElementById("playerName4").value];
+    players_global = [document.getElementById("playerName1").value, document.getElementById("playerName2").value, 
+    document.getElementById("playerName3").value,document.getElementById("playerName4").value];
 
     console.log("Spieler: ", players_global);
 
-    //liste_von_player
     let playerliste_html_ul = document.getElementById("liste_von_player");
 
-    
     players_global.forEach(ein_player_name => {
         const li = document.createElement("li");
         console.log("li: ", li);
@@ -45,6 +57,4 @@ document.getElementById('playerNamesForm').addEventListener('submit', function(e
 
         span.textContent=ein_player_name;
     });
-
-
 })
